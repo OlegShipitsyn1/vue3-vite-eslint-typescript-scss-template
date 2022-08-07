a1a3a83703b5a0fa6b01d090b2fb3a95c005ef94
Oleg Shipitsyn <olegshipitsyn.work@gmail.com>
2022-08-07 20:03:10 +0300

    feat(template): working template for vue 3 projects

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import BaseButton from '@/components/BaseButton.vue'
+</script>
+
+<template>
+  <main>
+    <h1>application</h1>
+    <BaseButton>Component</BaseButton>
+  </main>
+</template>
+
+<style lang="scss">
+$red: #b86767;
+main {
+  background-color: $red;
+  button {
+    color: $green;
+  }
+}
+</style>
